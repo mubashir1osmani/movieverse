7f3e486bb22c59a3b5b9c0af418ea8910bc45e7f
mubashir1osmani <ilikewafflesomcuh@gmail.com>
2024-10-30 01:26:22 -0400

    i just wanna sleep

--- movies-backend/src/main/java/gg/com/movies/WebConfig.java
+++ movies-backend/src/main/java/gg/com/movies/WebConfig.java
@@ -1,24 +1,16 @@
 package gg.com.movies;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
+public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                .allowedOrigins("https://moviessforuu.netlify.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
             }
-        };
     }
-}
--- movies-frontend/src/api/axiosconfig.js
+++ movies-frontend/src/api/axiosconfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_URL || "http://localhost:8080";
-
+const baseURL = process.env.REACT_APP_API_URL
+console.log("API BASE URL: ", baseURL)
 export default axios.create({
     baseURL: baseURL,
     headers: {"ngrok-skip-browser-warning": true,
